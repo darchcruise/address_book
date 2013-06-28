5ba63590c6280ecaf8f562f2bc8e0f7ad85e2626
darchcruise <darchcruise@yahoo.com>
2013-06-28 06:28:46 -0400

    main.rb-get do aka homepage

--- main.rb
+++ main.rb
@@ -4,3 +4,20 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pg'
 
+get '/' do     # home page
+  #create an array
+  @contacts = []
+  #open database
+  db =PG.connect(:dbname => 'address_book', :host => 'localhost')
+
+  db.exec("select * from contacts") do |result|
+  #create loop
+    result.each do |row|
+      @contacts << row
+    end
+  end
+#close database
+  db.close
+  erb :home_page
+end
+
